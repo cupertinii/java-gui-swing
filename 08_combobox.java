12b9de65f2b128c693466138909c56a2daf0b2c6
cupertinii <aditi.cupertino@outlook.com>
2020-11-07 12:20:43 -0800

    Added Combobox

---
+++ 08_combobox.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUIApp {
+  public static void main(String[] args) {
+
+    JFrame frame = new JFrame("Cupertinii Swing GUI");
+    frame.setSize(400, 500);
+    frame.setLayout(null);
+
+    JLabel label = new JLabel("Select a weekday");
+    label.setBounds(100, 20, 200, 20);
+
+    // Create array with all the options
+    String days[] = { "Monday", "Teusday", "Wednesday", "Thursday", "Friday" };
+
+    // Pass the array to ComboBox constructor
+    JComboBox cmbDays = new JComboBox(days);
+    cmbDays.setBounds(100, 50, 200, 20);
+
+    // This is how I can set the default entry
+    cmbDays.setSelectedItem( "Friday" );
+    System.out.println("Currently selected item is " + cmbDays.getSelectedIndex());
+
+    // Add all the components to application frame
+    frame.add(label);
+    frame.add(cmbDays);
+
+    frame.setVisible(true);
+  }
+}
