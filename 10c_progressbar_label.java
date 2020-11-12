4d3c49174679d186d259131657bf7a4c4244985c
cupertinii <aditi.cupertino@outlook.com>
2020-11-12 15:34:14 -0800

    Added example to show progress value

---
+++ 10c_progressbar_label.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUIApp {
+  public static void main(String[] args) {
+
+    // https://docs.oracle.com/javase/7/docs/api/javax/swing/JSlider.html
+
+    JFrame frame = new JFrame("Cupertinii Swing GUI");
+    frame.setSize(500, 500);
+    // I had to comment following line out to get the slider
+    // frame.setLayout(null);
+
+    frame.setLayout(null);
+    frame.setVisible(true);
+
+    progressBarHorizontalExample(frame);
+  }
+
+  public static void progressBarHorizontalExample(JFrame frame) {
+    // JSlider mySlider = new JSlider(JSlider.HORIZONTAL);
+    int min = 0;
+    int max = 100;
+    int current_value = 0;
+    JProgressBar myProgressBar = new JProgressBar(0, 100);
+
+    myProgressBar.setLocation(20, 20);
+    myProgressBar.setSize(200, 60);
+
+    myProgressBar.setValue(44);
+
+    JLabel label = new JLabel();
+    label.setBounds(250, 25, 100, 50);
+
+    frame.add(label);
+    frame.add(myProgressBar);
+
+    // Call the function to increment the progress bar...
+    startProgress(myProgressBar, label);
+  }
+
+  public static void startProgress(JProgressBar myProgressBar, JLabel label) {
+    int i = 0;
+    while (i <= 100) {
+      // Set the Progress bar value
+      myProgressBar.setValue(i);
+      label.setText( String.valueOf(i) + "%");
+      // Wait for a half a second
+      Wait(500);
+      // Increment the count by 10. This will increase the progress bar by 10
+      i += 10;
+    }
+  }
+
+  public static void Wait(int milliseconds) {
+    try {
+      Thread.sleep(milliseconds);
+    }
+    catch(InterruptedException ex) {
+      System.out.println("Error"+ ex);
+    }
+  }
+
+}
